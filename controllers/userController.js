4ff66915d2cec3e810a7ac3cfb7329efed5555f6
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-22 23:24:43 +0700

    Pindahkan logika pendaftaran pengguna ke controller terpisah. Pendaftaran menggunakan API Key ID yang divalidasi oleh middleware.

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+const db = require("../server_db");
+
+exports.createUser = (req, res) => {
+  const { first_name, last_name, email_address } = req.body;
+  const apiKeyRecord = req.apiKeyRecord;
+
+  db.query(
+    "INSERT INTO users (first_name, last_name, email_address, api_key_id, created_at) VALUES (?,?,?,?,NOW())",
+    [first_name, last_name, email_address, apiKeyRecord.id],
+    (err) => {
+      if (err) return res.status(500).json({ success: false });
+
+      res.json({ success: true, message: "User created" });
+    }
+  );
+};
